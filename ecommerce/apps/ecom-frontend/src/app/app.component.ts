fddad9450f6bf93e3ece11fa1fe06b9f023d0296
GrigorianK8 <karogrigorian8@gmail.com>
2024-09-30 21:53:21 +0400

    frontend part * created angular project using nx * added tailwindCss framework * added daisyUI component library * added font awesome

---
+++ ecommerce/apps/ecom-frontend/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FaConfig, FaIconComponent, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { fontAwesomeIcons } from './shared/font-awesome-icons';
+
+@Component({
+  standalone: true,
+  imports: [RouterModule, FaIconComponent],
+  selector: 'ecom-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent implements OnInit {
+  private faIconLibrary = inject(FaIconLibrary);
+  private faConfig = inject(FaConfig);
+
+  ngOnInit(): void {
+    this.initFontAwesome();
+  }
+
+  private initFontAwesome() {
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+    this.faConfig.defaultPrefix = 'far';
+  }
+}
